d67af92b2f8584006f09c64dc70ad6c34e72837e
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-15 13:26:49 +0000

    Add ErrorLogger tests

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -0,0 +1,39 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class ErrorLoggerTests
+    {
+        private ErrorLogger _logger;
+
+        [SetUp]
+        public void Setup()
+        {
+            _logger = new ErrorLogger();
+        }
+
+        [Test]
+        public void Log_WhenCalled_SetTheLastErrorProperty()
+        {
+            _logger = new ErrorLogger();
+            _logger.Log("a");
+
+            Assert.That(_logger.LastError, Is.EqualTo("a"));
+        }
+
+        [Test]
+        [TestCase(null)]
+        [TestCase("")]
+        [TestCase(" ")]
+        public void Log_InvalidStringProvided_ThrowArgumentNullException(string error)
+        {
+            _logger = new ErrorLogger();
+            Assert.That(() => _logger.Log(error), Throws.ArgumentNullException);
+        }
+    }
+}
