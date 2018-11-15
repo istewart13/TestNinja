186775ab25801b65a97989184ec15682a0d382d0
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-15 13:26:59 +0000

    Add fizz buzz tests

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public void GetOutput_NumberDivisableBy3Only_ReturnFizz()
+        {
+            var result = FizzBuzz.GetOutput(9);
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberDivisableBy5Only_ReturnBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberDivisableBy3And5_ReturnFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_NumberNotDivisableBy3Or5_ReturnInputAsString()
+        {
+            var result = FizzBuzz.GetOutput(7);
+            Assert.That(result, Is.EqualTo("7"));
+        }
+
+    }
+}
