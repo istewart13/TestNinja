001e3bef19cc92fe9264f7efd855b24a1673c75a
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-16 13:41:08 +0000

    Add DemeritPoint tests

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/DemeritPointsCalculatorTests.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class DemeritPointsCalculatorTests
+    {
+        [Test]
+        [TestCase(-1)]
+        [TestCase(301)]
+        public void CalculateDemeritPoints_OutOfRangeSpeed_ThrowsArgumentOutOfRangeException(int speed)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            Assert.That(() => demeritPointsCalculator.CalculateDemeritPoints(speed), Throws.Exception.TypeOf<ArgumentOutOfRangeException>());
+        }
+
+        [Test]
+        [TestCase(0)]
+        [TestCase(65)]
+        public void CalculateDemeritPoints_WithinSpeedLimit_Returns0(int speed)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            var result = demeritPointsCalculator.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(0));
+        }
+
+        [Test]
+        [TestCase(70, 1)]
+        [TestCase(75, 2)]
+        [TestCase(80, 3)]
+        public void CalculateDemeritPoints_WhenCalled_ReturnsDemeritPoints(int speed, int demeritPoints)
+        {
+            var demeritPointsCalculator = new DemeritPointsCalculator();
+            var result = demeritPointsCalculator.CalculateDemeritPoints(speed);
+            Assert.That(result, Is.EqualTo(demeritPoints));
+        }
+    }
+}
