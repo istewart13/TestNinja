f3c08cce20618f4991f48bc556bbb50443e3641c
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-21 15:09:01 +0000

    Setup VideoService for DI and add a moq test

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/VideoServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class VideoServiceTests
+    {
+        private Mock<IFileReader> _mockFileReader;
+        private VideoService _videoService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileReader = new Mock<IFileReader>();
+            _videoService = new VideoService(_mockFileReader.Object);
+        }
+
+        [Test]
+        public void ReadVideoTitle_NullFile_ReturnError()
+        {
+            _mockFileReader.Setup(fr => fr.Read("video.txt")).Returns("");
+
+            var result = _videoService.ReadVideoTitle();
+
+            Assert.That(result, Does.Contain("error").IgnoreCase);
+        }
+    }
+}
