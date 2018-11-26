d2804d6120d6285e2c15066556b781f5af7e7e59
Iain Stewart <iain.stewart@mcdermottgroup.co.uk>
2018-11-26 13:18:06 +0000

    Add test for Installer Helper

---
+++ source-code-starter/TestNinja/TestNinja.UnitTests/Mocking/InstallerHelperTests.cs
@@ -0,0 +1,41 @@
+﻿using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TestNinja.Mocking;
+
+namespace TestNinja.UnitTests.Mocking
+{
+    [TestFixture]
+    public class InstallerHelperTests
+    {
+        private Mock<IFileDownloader> _mockFileDownloader;
+        private InstallerHelper _installerHelper;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockFileDownloader = new Mock<IFileDownloader>();
+            _installerHelper = new InstallerHelper(_mockFileDownloader.Object);
+        }
+
+        [Test]
+        public void DownloadInstaller_UnableToDownloadFile_ReturnsFalse()
+        {
+            _mockFileDownloader.Setup(fd => fd.DownloadFile(It.IsAny<string>(), It.IsAny<string>())).Throws<WebException>();
+
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+
+            Assert.That(result, Is.EqualTo(false));
+        }
+
+        [Test]
+        public void DownloadInstaller_DownloadFileSuccessfully_ReturnsTrue()
+        {
+            var result = _installerHelper.DownloadInstaller("customer", "installer");
+            Assert.That(result, Is.True);
+        }
+    }
+}
